5bf56bd918127b5d26d133b6e7a2dbfe4d6dd92f
pablitomix-coder <62516017+pablitomix-coder@users.noreply.github.com>
2022-05-27 09:51:59 -0500

    Correcion de barra menu

--- assets/js/utilidades.js
+++ assets/js/utilidades.js
@@ -1,15 +1,22 @@
 let menuDesplegado = false;
-let longMenu = $(".barra").height();
+let longMenu = $(window).width();
 $("#iconmenu").on('click', function() {
     if (menuDesplegado) {
         $("#menu").css("display", "none");
-        if (longMenu < 90) {
+        if (longMenu < 500) {
             $(".barra").css("height", "20vw")
+        } else if (longMenu >= 500 && longMenu < 576) {
+            $(".barra").css("height", "18vw")
+        } else if (longMenu >= 576 && longMenu < 768) {
+            $(".barra").css("height", "16vw")
+        } else if (longMenu >= 768) {
+            $(".barra").css("height", "13vw")
         }
         menuDesplegado = false;
     } else {
         $("#menu").css("display", "block");
-        console.log($(".barra").height());
+        longMenu = $(window).width();
+        console.log(longMenu);
         $(".barra").css("height", "100vh");
         menuDesplegado = true;
         console.log("desplegar");
